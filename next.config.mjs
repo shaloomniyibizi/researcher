dede5e7e1cf4723fb98c91733490944fac1e411c
Shaloom <shaloomniyibizi@gmail.com>
2024-08-25 02:44:30 +0200

    erth

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   webpack: (config, { isServer }) => {
+    config.resolve.alias['@langchain/core'] = require.resolve('@langchain/core');
+    return config;
+  },
     eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
